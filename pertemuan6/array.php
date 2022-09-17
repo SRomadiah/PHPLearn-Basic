616f6db71aff20b9803ffe50e725ab8c3ea1d250
SRomadiah <diyah1888@gmail.com>
2022-09-18 07:49:30 +0800

    lanjutan dari pertemuan ke 5

---
+++ pertemuan6/array.php
@@ -0,0 +1,18 @@
+<?php
+// materi saat ini adalah lanjutan dari pertemuan ke 5
+// review membuat array 
+
+// cara lama 
+$hari = array("Senin", "Selasa", "Rabu");
+$bulan = ["Jan", "Feb", "Mart"];
+$arr = [123, "teks", true];
+
+// menampilkan array 
+
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+
+// menampilkan 1 elemen array 
+echo $hari[2];
