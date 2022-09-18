f56f15d6589b80c82981abcaf122407d76a99057
SRomadiah <diyah1888@gmail.com>
2022-09-18 17:47:57 +0800

    ini adalah latihan tentang array associative

--- pertemuan6/latihan.php
+++ pertemuan6/latihan.php
@@ -6,6 +6,24 @@
 // array associative 
 // definisinya sama seperti array numerik
 // key-nya adalah strng yang kita buat 
+// dlam array associative di perbolehkan tidak sesuai urutan asalkan key stringnya tepat
+$mahasiswa = [
+  [
+    "Nama" => "Romadiah",
+    "NIM" => "21101198",
+    "Email" => "romadiah@gmail.com",
+    "Jurusan" => "Teknologi Informatika",
+    "Gambar" => "photo1.jpg"
+  ],
+  [
+    "Nama" => "Taniamelia",
+    "NIM" => "21101176",
+    "Email" => "taniamelia@gmail.com",
+    "Jurusan" => "Teknologi Informatika",
+    "Gambar" => "photo2.jpg"
+  ]
+];
+// echo $mahasiswa[1]["Nilai"][2];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,10 +39,13 @@
   <h1>Daftar Mahasiswa </h1>
   <?php foreach ($mahasiswa as $data) : ?>
     <ul>
-      <li>Nama: <?= $data[0]; ?></li>
-      <li>NIM: <?= $data[1]; ?></li>
-      <li>Email: <?= $data[2]; ?></li>
-      <li>Jurusan: <?= $data[3]; ?></li>
+      <li>
+        <img src="img/<?= $data["Gambar"]; ?>">
+      </li>
+      <li>Nama: <?= $data["Nama"]; ?></li>
+      <li>NIM: <?= $data["NIM"]; ?></li>
+      <li>Email: <?= $data["Email"]; ?></li>
+      <li>Jurusan: <?= $data["Jurusan"]; ?></li>
     </ul>
   <?php endforeach; ?>
 </body>
